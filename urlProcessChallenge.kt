d80dc40620d8975d181cce6f60c9b1c404efe51f
Rodrigo Freire <112009439+RodrigoFreireA@users.noreply.github.com>
2023-09-27 13:00:59 -0300

    Create urlProcessChallenge.kt

---
+++ urlProcessChallenge.kt
@@ -0,0 +1,39 @@
+fun main() {
+    val urls = mutableListOf<String>()
+
+    while (true) {
+        val input = readLine() ?: break
+        if (input.isBlank()) break
+        urls.add(input)
+    }
+
+    println("Iniciando downloads...")
+
+    val results = mutableListOf<Pair<Int, Int>>()
+
+    val threads = urls.mapIndexed { index, url ->
+        Thread {
+            val length = openLink(url)
+            synchronized(results) {
+                results.add(Pair(index, length))
+            }
+        }
+    }
+
+    // Inicie cada Thread para começar o processo de "download" paralelamente.
+    threads.forEach { it.start() }
+
+    // Aguarde até que todas as Threads terminem suas respectivas execuções.
+    threads.forEach { it.join() }
+
+    // Sort results by index to print in the correct order
+    results.sortedBy { it.first }.forEachIndexed { idx, result ->
+        println("Arq${idx + 1}: ${result.second}")
+    }
+
+    println("Tempo total: ${urls.size}")
+}
+
+fun openLink(url: String): Int {
+    return url.length
+}
